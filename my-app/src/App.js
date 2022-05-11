089710cb3e7638d0d61cfcc6fe0062855c9fda71
fidyahptr <fidyahputri15@gmail.com>
2022-05-11 18:59:53 +0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
 import Form from './pages/Form';
 
 function App() {
